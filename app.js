51e74d252777feef1b1b52086a411d47670852ea
JUSTIN CHEN <justinchen90@gmail.com>
2017-05-10 04:20:51 -0400

    Some polishing

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ window.onload = function init() {
 	navigator.mediaDevices.getUserMedia({audio: true, video: false}).then(function(stream){
 		var source = AUDIO_CONTEXT.createMediaStreamSource(stream);
 		source.connect(ANALYSER);
-		visualize();
+		visualizeFrequencyBars();
 	}).catch(function(err){
 		console.log(err.name + ": " + err.message)
 	});
@@ -85,7 +85,7 @@ window.onload = function init() {
 		}
 		else{
 			noUiSlider.create(freq_bars_sliders[i],{
-				start: 80,
+				start: 120,
 				step: 1,
 				range: {
 					'min': 0,
@@ -99,12 +99,49 @@ window.onload = function init() {
 		freq_bars_sliders[i].noUiSlider.on('update', setFreqBarsColors);
 	}
 
+
+	/*
+		Preset 1:
+		grd.addColorStop(0, 'rgba(100, 200, 230, 1)');
+		grd.addColorStop(1, 'rgba(102, 102, 255, 0.5)');
+		canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ',100,250)';
+	*/
+
+	const BARS_PRESETS = document.getElementsByClassName('bars-presets');
+
+	//	1980's Miami Preset
+	BARS_PRESETS[0].addEventListener('click', function(){
+		freq_bars_sliders[0].noUiSlider.set(100);
+		freq_bars_sliders[1].noUiSlider.set(200);
+		freq_bars_sliders[2].noUiSlider.set(230);
+		freq_bars_sliders[3].noUiSlider.set(1.0);
+		freq_bars_sliders[4].noUiSlider.set(102);
+		freq_bars_sliders[5].noUiSlider.set(102);
+		freq_bars_sliders[6].noUiSlider.set(255);
+		freq_bars_sliders[7].noUiSlider.set(0.5);
+		freq_bars_sliders[8].noUiSlider.set(85);
+		freq_bars_sliders[9].noUiSlider.set(100);
+		freq_bars_sliders[10].noUiSlider.set(250);
+		freq_bars_sliders[11].noUiSlider.set(1.0);
+		for(let i=0; i<BARS_PRESETS.length; i++){
+			BARS_PRESETS[i].className = "bars-presets generic-menu-item";
+		}
+		BARS_PRESETS[0].className = "bars-presets generic-menu-item selected";
+	});
+
+	//	Manually default to this preset
+	BARS_PRESETS[0].click();
 }
 
 function setFreqBarsColors(){
 	for(let i=0; i<freq_bars_sliders.length; i++){
 		if(typeof freq_bars_sliders[i].noUiSlider != 'undefined'){
-			freq_bars_values[i].innerHTML = freq_bars_sliders[i].noUiSlider.get();
+			if(i === 3 || i === 7 || i === 11){
+				freq_bars_values[i].innerHTML = freq_bars_sliders[i].noUiSlider.get();
+			}
+			else{
+				freq_bars_values[i].innerHTML = Math.round(freq_bars_sliders[i].noUiSlider.get());
+			}
 		}
 	}
 }
@@ -116,7 +153,7 @@ function resizeCanvas(){
 }
 
 //
-function visualize(){
+function visualizeFrequencyBars(){
 
 	//	Max size: 32768
 	//	Must be a power of 2
@@ -154,40 +191,39 @@ function visualize(){
 		var barHeight;
 		var x = 0;
 
-		// Initialize the gradient color for the background
-		const grd = canvasCtx.createLinearGradient(0, 0, 170, canvasCtx.canvas.height);
-
 		//	Get the color values from the slider settings
 		//	Background 1 RGBA colors
 		let background1_r = Math.round(freq_bars_sliders[0].noUiSlider.get());
 		let background1_g = Math.round(freq_bars_sliders[1].noUiSlider.get());
 		let background1_b = Math.round(freq_bars_sliders[2].noUiSlider.get());
-		let background1_a = Math.round(freq_bars_sliders[3].noUiSlider.get());
+		let background1_a = freq_bars_sliders[3].noUiSlider.get();
 		//	Background 2 RGBA colors
 		let background2_r = Math.round(freq_bars_sliders[4].noUiSlider.get());
 		let background2_g = Math.round(freq_bars_sliders[5].noUiSlider.get());
 		let background2_b = Math.round(freq_bars_sliders[6].noUiSlider.get());
-		let background2_a = Math.round(freq_bars_sliders[7].noUiSlider.get());
+		let background2_a = freq_bars_sliders[7].noUiSlider.get();
+		//	Bar Color RGBA colors
+		let barColor_r = Math.round(freq_bars_sliders[8].noUiSlider.get());
+		let barColor_g = Math.round(freq_bars_sliders[9].noUiSlider.get());
+		let barColor_b = Math.round(freq_bars_sliders[10].noUiSlider.get());
+		let barColor_a = freq_bars_sliders[11].noUiSlider.get();
 
+		// Initialize the gradient color for the background
+		const grd = canvasCtx.createLinearGradient(0, 0, 170, canvasCtx.canvas.height);
+
+		//	Color the background
 		grd.addColorStop(0, 'rgba('+ background1_r +', '+ background1_g +', '+ background1_b +', '+ background1_a +')');
 		grd.addColorStop(1, 'rgba('+ background2_r +', '+ background2_g +', '+ background2_b +', '+ background2_a +')');
-
-		/*
-			Preset 1:
-			grd.addColorStop(0, 'rgba(100, 200, 230, 1)');
-			grd.addColorStop(1, 'rgba(102, 102, 255, 0.5)');
-			canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ',100,250)';
-		*/
 
 		// Apply the gradient to the next rectangle to be drawn, then set it as the background
 		canvasCtx.fillStyle = grd;
 		canvasCtx.fillRect(0, 0, canvasCtx.canvas.width, canvasCtx.canvas.height);
 
 		for(var i = 0; i < bufferLength; i++){
-			barHeight = dataArray[i] * 1.4 + 10;
+			barHeight = Math.round(dataArray[i] * 1.4 + 10);
 
 			//	RGB needs to take integers, so we need to round 'barHeight'.
-			canvasCtx.fillStyle = 'rgb(' + (Math.round(barHeight)+100) + ',100, 250)';
+			canvasCtx.fillStyle = 'rgba(' + (dataArray[i] + barColor_r + 10) + ','+ barColor_g +', '+ barColor_b +', '+ barColor_a +')';
 			canvasCtx.fillRect(x,canvasCtx.canvas.height-barHeight/2-canvasCtx.canvas.height/2, barWidth, barHeight);
 
 			x += barWidth + 1;
